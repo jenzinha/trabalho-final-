817fd5da085acb3d8cc825eb6b2821ba2ba34fb7
jenniekkj <101286269+jenzinha@users.noreply.github.com>
2022-06-15 15:31:39 +0000

    tentando fazer o erro customizado

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-
-
-// let requestURL = "https://www.luizpicolo.com.br/api.json";
-// let request = new XMLHttpRequest();
-//   request.open("GET", requestURL)
-//    request.responseType = 'json';
-//    request.send();
-
-//    request.onload = function(){
-//   let resposta = request.response;
-// const elemento = document.getElementById('lista');
-// let noticias = JSON.parse(resposta)
-
-// let titulo = `<h1>Noticias</h1>`;
-//   elemento.insertAdjacentHTML('afterbegin', titulo);
-
-//   resposta.articles.forEach(noticias => {
-//     let nome = `<div class="noticias">${noticias.source.title}</div>`;
-//     elemento.insertAdjacentHTML('beforeend', nome);
-//   });
-// }
-
-
-//  let titulodaNot = noticias.articles[0].title  
-//  let datadaNot = noticias.articles[0].publishedAt 
-//  let autordaNot = noticias.articles[0].author
-//  let resumodaNot = noticias.articles[0].description
-  
-
-    
-//    let noticia = new Noticia(titulodaNot, datadaNot, autordaNot, resumodaNot);
-//    console.log(noticia.mostrarNoticia())
-
-
 let requisicaoURL = 'https://www.luizpicolo.com.br/api.json';
 let request = new XMLHttpRequest();
 request.open('get', requisicaoURL);
@@ -39,60 +5,118 @@ request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-  
+   class ErroCostumizado extends Error{
+   constructor(nome, mensagem){
+     super(nome, mensagem)
+     this.name = nome;
+     this.message = mensagem;
+   }
+     mensagem(){
+       let linha1 = this.stack.split("\n")[1]
+       let linha2 = linha1.split(":")[1]
+       let linha3 = linha2.split(")")[0]
+       
+     return this.name + this.message + "\n" + "Há um erro na linha " + linha3
+      }
+  }
    class Noticias{
-   constructor(nome, titulo, data_da_publi, autor, resumo, link){
-    
+   constructor(nome, link, titulo, data_da_publi, autor){
+ 
     this.nome = nome;
+    this.link = link;
     this.titulo = titulo;
     this.data_da_publi = data_da_publi;
     this.autor = autor;
-    this.resumo = resumo;
-    this.link = link;
   }
    
   mostrarNoticia(){
-    return `
-      <h1>${this.titulo} </h1> 
-      <p> ${this.data_da_publi}</p>
-      <p> ${this.autor}</p>
-      <p> ${this.resumo}</p>
+    
+    if(this.nome && this.link && this.titulo && this.data_da_publi && this.autor == undefined ){
+       throw new ErroCostumizado("Erro:", "Faltam campos a serem preenchidos.")
+      }
+     else{
+       return `
+<center>
+<div class="bg-secondary bg-opacity-50 rounded" style="max-width: 50rem;">
+ <h5>${this.nome}</h5> 
+  <h5>Publicado em:${this.data_da_publi}</h5>
+  <a class="text-decoration-none text-dark" href="${this.link}"> <h1 class="m-4" class="fs 3">${this.titulo} </h1> </a>
+  <h5> ${this.autor}</h5>
+</div>
+</center>
 `
-  }
-}
+     }
+    }
+   }
   
 class NoticiaDestaque extends Noticias{
-  constructor(imagem, titulo, data_da_publi, autor, resumo){
-    super(titulo, data_da_publi, autor, resumo)
+  constructor(imagem, nome, link, titulo, data_da_publi, autor, resumo){
+    super(nome, link, titulo, data_da_publi, autor);
     this.imagem = imagem;
+    this.resumo = resumo;
   }
   
   mostrarDestaque(){
-    return `<img src="https://techcrunch.com/wp-content/uploads/2021/06/GettyImages-186872728.jpg?w=600">${this.imagem} 
-            <h1>${this.titulo} </h1> 
-      <p> ${this.data_da_publi}</p>
-      <p> ${this.autor}</p>
-      <p> ${this.resumo}</p>
-`  
+     
+    if(this.imagem && this.nome && this.link && this.titulo  && this.data_da_publi && this.autor && this.resumo == undefined){
+       throw new ErroCostumizado("Erro:", "Faltam campos a serem preenchidos.")
+      }
+     else{
+      return `
+   <h1 class="text-center"> Notícias <h1>
+  <div class="text-center mb-3" >
+  <img class="rounded img-fluid" src="${this.imagem}">
+  <h5>${this.nome}</h5>
+  <h5>Publicado em: ${this.data_da_publi} </h5>
+  <a class="text-decoration-none text-dark" href="${this.link}"> <h1 class="fs-1">${this.titulo} </h1> </a>
+  <h5> ${this.autor}</h5>
+  <center> <p class="fs-4 fw-lighter w-50"> ${this.resumo}</p> </center>
+</div>
+`
+    }
   }
-}
-  
+} 
   let resposta = request.response;
-  console.log(resposta)
   const elemento = document.getElementById('lista');
+  console.log(resposta)
+   
+      
+  resposta.articles.forEach(noticia => {
+  let noticiaa = new Noticias(noticia.source.name, noticia.url, noticia.title, noticia.publishedAt, noticia.author)
+  elemento.insertAdjacentHTML('afterbegin', noticiaa.mostrarNoticia())
+ })
+    let noticiasDestaque = new NoticiaDestaque(resposta.articles[0].urlToImage,
+                                               resposta.articles[0].source.name,
+                                               resposta.articles[0].url,
+                                               resposta.articles[0].title,
+                                               resposta.articles[0].publishedAt,  
+                                               resposta.articles[0].author, 
+                                               resposta.articles[0].description)
+    elemento.insertAdjacentHTML('afterbegin', noticiasDestaque.mostrarDestaque())
+}
+
+
+
+
+
+// class Pessoa{
+  
+// }
+
+// class Fornecedor extends Pessoa{
+//   constructor(credito, divida, empresa){
+//     super();
+//     this.valorCredito = credito;
+//     this.valorDivida = divida;
+//     this.empresa = empresa;
+//   }
 
   
-      let noticiasDestaque = new NoticiaDestaque(resposta.urlToImage, resposta.title, resposta.author, resposta.publishedAt, resposta.author, resposta.description)
-    elemento.insertAdjacentHTML('afterbegin', noticiasDestaque.mostrarDestaque())
-
-  resposta.articles.forEach(noticia => {
+//  }
 
 
-    let noticiaa = new Noticias(noticia.title, noticia.author, noticia.publishedAt, noticia.author, noticia.description)
-    elemento.insertAdjacentHTML('afterbegin', noticiaa.mostrarNoticia())
 
 
-    
     // let titulo= `<div class="noticia">${noticia.title}</div>`;
     // elemento.insertAdjacentHTML('beforeend', titulo);
 
@@ -105,5 +129,3 @@ class NoticiaDestaque extends Noticias{
     // let descricao= `<div class="noticia">${noticia.description}</div>`;
     // elemento.insertAdjacentHTML('beforeend', descricao);
 
-  });
-}
